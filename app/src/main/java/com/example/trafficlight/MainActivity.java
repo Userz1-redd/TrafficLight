542d06e8524349663aa8da7bfb7070620498bfe5
kangyunsik <re12io@ajou.ac.kr>
2021-03-05 22:34:24 +0900

    test

--- app/src/main/java/com/example/trafficlight/MainActivity.java
+++ app/src/main/java/com/example/trafficlight/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //adbcd
+        //adbcdadsfadf
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
