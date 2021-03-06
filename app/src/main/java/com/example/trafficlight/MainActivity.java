8a09232fdd87e3b09eeea68293bb014c52d9f873
Userz1-redd <credit_@naver.com>
2021-03-06 15:15:54 +0900

    sdsdsads

--- app/src/main/java/com/example/trafficlight/MainActivity.java
+++ app/src/main/java/com/example/trafficlight/MainActivity.java
@@ -1,21 +1,18 @@
 package com.example.trafficlight;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+
 public class MainActivity extends AppCompatActivity {
-//sssssfafasfa
+
     @Override
-    //asdasdsad
-    //2222
     protected void onCreate(Bundle savedInstanceState) {
-        //sdf
-        //sdfasdf
-        //added by ys.
-        //add to master
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
 }
-//sss
